4e156c79248728c4caf2b898e067c8ef3070dec7
Ariel <arielladret@gmail.com>
2019-11-08 13:46:39 -0800

    init

---
+++ themes/inhabitent/inc/extras.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Custom functions that act independently of the theme templates.
+ *
+ * @package RED_Starter_Theme
+ */
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes Classes for the body element.
+ * @return array
+ */
+function red_starter_body_classes( $classes ) {
+	// Adds a class of group-blog to blogs with more than 1 published author.
+	if ( is_multi_author() ) {
+		$classes[] = 'group-blog';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'red_starter_body_classes' );
+
+function my_login_logo_one() { 
+	?> 
+	<style type="text/css"> 
+	body.login div#login h1 a {
+	 background-image: url("<?php echo get_stylesheet_directory_uri(); ?>/images/images/logos/inhabitent-logo-tent.svg"); 
+	padding-bottom: 30px; 
+	} 
+	</style>
+	<?php 
+} 
+
+add_action( 'login_enqueue_scripts', 'my_login_logo_one' );
+add_filter('login_hearderurl', 'my_custom_login_url'); 
+function my_custom_login_url($url) {
+
+	return 'http://localhost:8888/Inhabitent/'; 
+
+}
+
