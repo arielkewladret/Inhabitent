e977b69946b52e84c438de0b6fc26a13c3cc4001
Ariel <arielladret@gmail.com>
2019-11-16 16:28:24 -0800

    footer styling

--- themes/inhabitent/functions.php
+++ themes/inhabitent/functions.php
@@ -94,6 +94,8 @@ function red_starter_scripts() {
 	wp_enqueue_style( 'red-starter-font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.css');
 
 
+
+	wp_enqueue_script( 'red-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'red-starter-navigation', get_template_directory_uri() . '/build/js/navigation.min.js', array(), '20151215', true );
 	wp_enqueue_script( 'red-starter-skip-link-focus-fix', get_template_directory_uri() . '/build/js/skip-link-focus-fix.min.js', array(), '20151215', true );
 
--- themes/inhabitent/page-about.php
+++ themes/inhabitent/page-about.php
@@ -9,16 +9,28 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-         
+
+		<?php the_title(); ?>
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+				<?php get_template_part( 'template-parts/content', 'page-about' ); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
-        </main> 
-        
+		</main><!-- #main -->
 	</div><!-- #primary -->
 
+	<?php
+   $args = array( 'post_type' => 'product', 'order' => 'ASC' );
+   $product_posts = get_posts( $args ); // returns an array of posts
+?>
+<?php if(count($product_posts)> 0):?>
+<?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>
+   <?php /* Content from your array of post results goes here */ ?>
+<?php endforeach; wp_reset_postdata(); ?>
+<?php else:?> 
+<?php endif; ?>
+
+<!-- <?php get_sidebar(); ?> -->
 <?php get_footer(); ?>
